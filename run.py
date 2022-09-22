fc6f7341811e8bcc155fff8b14ad6f0cce0ffa83
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-09-22 19:22:06 +0000

    Added function to make the system choose a random word from the list

--- run.py
+++ run.py
@@ -14,6 +14,15 @@ words = [
 ]
 correct_letters = []
 incorrect_letters = []
-tries = 6 
-right_answers = 0 
+tries = 6
+right_answers = 0
 game_over = False
+
+"""This function will make the system choose a random word from the list."""
+
+
+def choose_word(list_of_words):
+    chosen_word = choice(list_of_words)
+    different_letters = len(set(chosen_word))
+
+    return chosen_word, different_letters
