48b00027a67e0ecc51812de0554207dc92c0d8cf
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-09-23 19:11:07 +0000

    Added functon to replace a letter with a dash for user to choose

--- run.py
+++ run.py
@@ -18,7 +18,8 @@ tries = 6
 right_answers = 0
 game_over = False
 
-"""This function will make the system choose a random word from the list."""
+"""This function will make the system
+choose a random word from the list."""
 
 
 def choose_word(list_of_words):
@@ -28,7 +29,8 @@ def choose_word(list_of_words):
     return chosen_word, different_letters
 
 
-"""The user won't get out of the loop until they enter a valid letter."""
+"""The user won't get out of the loop
+until they enter a valid letter."""
 
 
 def ask_letter():
@@ -45,3 +47,20 @@ def ask_letter():
             print("You haven't chosen a correct letter")
 
     return chosen_letter
+
+
+"""Replacing each letter with a dash so
+that the user has to guess which letter
+goes on each dash."""
+
+
+def show_new_board(choose_word):
+    hidden_list = []
+
+    for letter in choose_word:
+        if letter in correct_letters:
+            hidden_list.append(letter)
+        else:
+            hidden_list.append("-")
+
+    print(" ".join(hidden_list))
