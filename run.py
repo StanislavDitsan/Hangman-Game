3b0340152e35275c8690bc3fceb11c29852af3e0
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-09-23 18:42:38 +0000

    Added function to ask for a letter

--- run.py
+++ run.py
@@ -26,3 +26,22 @@ def choose_word(list_of_words):
     different_letters = len(set(chosen_word))
 
     return chosen_word, different_letters
+
+
+"""The user won't get out of the loop until they enter a valid letter."""
+
+
+def ask_letter():
+    chosen_letter = ""
+    is_valid = False
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    while not is_valid:
+        chosen_letter = input("Please enter a letter!")
+
+        if chosen_letter in alphabet and len(chosen_letter) == 1:
+            is_valid = True
+        else:
+            print("You haven't chosen a correct letter")
+
+    return chosen_letter
